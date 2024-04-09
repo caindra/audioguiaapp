b5af4ad512c796e0e16d679c0c99e3c9e72d3a1e
mmiralin <mmigal868@g.educaand.es>
2024-04-09 13:33:57 +0200

    created User + UserRepository

---
+++ src/Entity/User.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $fullName = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $password = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $isAdmin = false;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $isEditor = false;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFullName(): ?string
+    {
+        return $this->fullName;
+    }
+
+    public function setFullName(?string $fullName): User
+    {
+        $this->fullName = $fullName;
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): User
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(?string $password): User
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    public function getIsAdmin(): ?bool
+    {
+        return $this->isAdmin;
+    }
+
+    public function setIsAdmin(?bool $isAdmin): User
+    {
+        $this->isAdmin = $isAdmin;
+        return $this;
+    }
+
+    public function getIsEditor(): ?bool
+    {
+        return $this->isEditor;
+    }
+
+    public function setIsEditor(?bool $isEditor): User
+    {
+        $this->isEditor = $isEditor;
+        return $this;
+    }
+
+}
