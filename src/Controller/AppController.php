3daf55d80887c462d9d9ea92ab879010b7f3ad9e
mmiralin <mmigal868@g.educaand.es>
2024-04-11 13:30:53 +0200

    modified name principal route

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AppController extends AbstractController
 {
-    #[Route('/', name: 'portada')]
+    #[Route('/', name: 'cover')]
     public function cover() : Response
     {
         return $this->render('cover.html.twig');
