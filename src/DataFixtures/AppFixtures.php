2f865bee0738fed5bd062f86361b12ed680aca3e
mmiralin <mmigal868@g.educaand.es>
2024-05-16 11:25:57 +0200

    created fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\AudioguideFactory;
+use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +11,22 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        AudioguideFactory::createMany(5);
+
+        UserFactory::createOne([
+            'email' => 'correo@email.com',
+            'fullName' => 'Paco Martínez Sánchez',
+            'password' => 'editor',
+            'isEditor' => true,
+            'isAdmin' => false
+        ]);
+
+        UserFactory::createOne([
+            'email' => 'admin@email.com',
+            'fullName' => 'Sheldon Cooper',
+            'password' => 'admin',
+            'isAdmin' => true
+        ]);
 
         $manager->flush();
     }
